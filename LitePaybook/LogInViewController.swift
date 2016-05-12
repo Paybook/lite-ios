f2910752703093ecac32e450fea29605d17e4ad4
Gabriel Villarreal <gabriel@paybook.me>
2016-05-11 22:51:42 -0500

    First commit in order to create Xcode project.

---
+++ LitePaybook/LogInViewController.swift
@@ -0,0 +1,96 @@
+//
+//  LogInViewController.swift
+//  LitePaybook
+//
+//  Created by Gabriel Villarreal on 11/05/16.
+//  Copyright © 2016 Gabriel Villarreal. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func didPressEnter(sender: AnyObject) {
+        logIn()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func logIn(){
+        
+        let addr = getIFAddresses()
+            print(addr)
+            Alamofire.request(.POST, "http://\(addr[0]):5000/login", parameters: ["username": userTextField.text!, "password" : passwordTextField.text!], encoding: .JSON , headers: ["Content-Type": "application/json; charset=utf-8"]).validate()
+                .responseJSON { (response) -> Void in
+                    guard response.result.isSuccess else {
+                        print("Error while fetching remote rooms: \(response.result.error)")
+                        
+                        return
+                    }
+                    
+                    print(response.result)
+            }
+        
+        
+    }
+    
+    func getIFAddresses() -> [String] {
+        var addresses = [String]()
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs> = nil
+        if getifaddrs(&ifaddr) == 0 {
+            
+            // For each interface ...
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr.memory.ifa_next }
+                
+                let flags = Int32(ptr.memory.ifa_flags)
+                var addr = ptr.memory.ifa_addr.memory
+                
+                // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+                        
+                        // Convert interface address to a human readable string:
+                        var hostname = [CChar](count: Int(NI_MAXHOST), repeatedValue: 0)
+                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
+                            nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                            if let address = String.fromCString(hostname) {
+                                addresses.append(address)
+                            }
+                        }
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        
+        return addresses
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
